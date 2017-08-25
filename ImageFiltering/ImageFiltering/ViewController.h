33ee9594daa6e0e153a900c92070cba3d214a023
melanu1991 <melanu284733@gmail.com>
2017-08-25 10:34:58 +0300

    create project

---
+++ ImageFiltering/ImageFiltering/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageFiltering
+//
+//  Created by melanu1991 on 25.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageFiltering/ImageFiltering/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageFiltering
+//
+//  Created by melanu1991 on 25.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
